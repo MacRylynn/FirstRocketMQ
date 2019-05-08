4ecf5915dc85f7447c1ba4d9e8e05997a82eed20
Macrylynn <530454062@qq.com>
2019-05-08 13:31:13 +0800

    第一个RocketMQ项目，基于windows，本地安装RocketMQ 4.4.0版本

---
+++ src/main/java/com/example/demo/Producer.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * @Author: Rylynn
+ * @Date: 2019/5/8 11:33
+ * @Version 1.0
+ */
+public class Producer {
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("please_rename_unique_group_name");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("localhost:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 10; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            try {
+                SendResult sendResult = producer.send(msg);
+                System.out.printf("%s%n", sendResult);
+            }catch (Exception e){
+                System.out.println(e.toString());
+            }
+
+
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
